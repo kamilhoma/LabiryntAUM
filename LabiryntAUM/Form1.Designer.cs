752e0cea622bf2db32debd14b6ac08ed7d419bbe
kamilhoma <kamil.homa@gmail.com>
2016-01-26 22:49:21 +0100

    up

---
+++ LabiryntAUM/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace LabiryntAUM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawingArea = new System.Windows.Forms.PictureBox();
+            this.btDraw = new System.Windows.Forms.Button();
+            this.btn_przejdz = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.drawingArea)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawingArea
+            // 
+            this.drawingArea.Location = new System.Drawing.Point(118, 12);
+            this.drawingArea.Name = "drawingArea";
+            this.drawingArea.Size = new System.Drawing.Size(620, 614);
+            this.drawingArea.TabIndex = 0;
+            this.drawingArea.TabStop = false;
+            // 
+            // btDraw
+            // 
+            this.btDraw.Location = new System.Drawing.Point(12, 12);
+            this.btDraw.Name = "btDraw";
+            this.btDraw.Size = new System.Drawing.Size(100, 23);
+            this.btDraw.TabIndex = 1;
+            this.btDraw.Text = "Rysuj labirynt";
+            this.btDraw.UseVisualStyleBackColor = true;
+            this.btDraw.Click += new System.EventHandler(this.btDraw_Click);
+            // 
+            // btn_przejdz
+            // 
+            this.btn_przejdz.Location = new System.Drawing.Point(13, 42);
+            this.btn_przejdz.Name = "btn_przejdz";
+            this.btn_przejdz.Size = new System.Drawing.Size(99, 23);
+            this.btn_przejdz.TabIndex = 2;
+            this.btn_przejdz.Text = "Przejdź labirynt";
+            this.btn_przejdz.UseVisualStyleBackColor = true;
+            this.btn_przejdz.Click += new System.EventHandler(this.btn_przejdz_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 638);
+            this.Controls.Add(this.btn_przejdz);
+            this.Controls.Add(this.btDraw);
+            this.Controls.Add(this.drawingArea);
+            this.Name = "Form1";
+            this.Text = "Labirynt";
+            ((System.ComponentModel.ISupportInitialize)(this.drawingArea)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox drawingArea;
+        private System.Windows.Forms.Button btDraw;
+        private System.Windows.Forms.Button btn_przejdz;
+    }
+}
+
---
+++ LabiryntAUM/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabiryntAUM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            drawArea = drawingArea.CreateGraphics();
+        }
+
+        
+        Graphics drawArea;
+
+        private void btDraw_Click(object sender, EventArgs e)
+        {
+            List<string> z = czytajDane();
+            foreach (var item in z)
+            {
+                var spl = item.Split(' ');
+                rysuj(Int32.Parse(spl[0].ToString()), Int32.Parse(spl[1].ToString()), Int32.Parse(spl[2].ToString()), z);
+            }
+
+        }
+
+        private void btn_przejdz_Click(object sender, EventArgs e)
+        {
+            lecimy(czytajDane());
+        }
+
+        public List<string> czytajDane()
+        {
+            List<string> dane = new List<string>();
+            string lina;
+
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"D:\projekty\Labirynt AUM\mazeData4x4.txt");
+            while ((lina = file.ReadLine()) != null)
+            {
+                 dane.Add(lina);
+            }
+
+            file.Close();
+            return dane;
+
+        }
+
+        #region rysuj
+        public void rysuj(int x, int y, int instrukcja, List<string> dane)
+        {
+            string last = dane.Last().ToString();
+            var spl = last.Split(' ');
+            int xmax = Int32.Parse(spl[0].ToString());
+            int ymax = Int32.Parse(spl[1].ToString());
+
+            Pen pen = new Pen(Color.Black);
+            Pen pen2 = new Pen(Color.Red);
+            switch (instrukcja)
+            {
+                case 0:
+                    drawArea.DrawLine(pen, skaluj(x), skaluj(y), skaluj(x), skaluj(y+1));
+                    drawArea.DrawLine(pen, skaluj(x), skaluj(y), skaluj(x+1), skaluj(y));
+                    break;
+                case 1:
+                    drawArea.DrawLine(pen, skaluj(x), skaluj(y), skaluj(x), skaluj(y+1));
+                    break;
+                case 2:
+                    drawArea.DrawLine(pen, skaluj(x), skaluj(y), skaluj(x+1), skaluj(y));
+                    break;
+                case 3:
+                    break;
+            }
+            drawArea.DrawLine(pen, skaluj(0), skaluj(0), skaluj(xmax+1), skaluj(0));
+            drawArea.DrawLine(pen, skaluj(0), skaluj(0), skaluj(0), skaluj(ymax+1));
+            drawArea.DrawLine(pen, skaluj(xmax+1), skaluj(ymax+1), skaluj(xmax+1), skaluj(0));
+            drawArea.DrawLine(pen, skaluj(xmax + 1), skaluj(ymax + 1), skaluj(0), skaluj(ymax+1));
+        }
+
+        public float skaluj(float x)
+        {
+            int s = 38;
+            x = x * s;
+            return x;
+        }
+        #endregion
+
+        #region przejdz
+        public void lecimy(List<string> dane)
+        {
+            foreach (var item in dane)
+            {
+                //Debug.WriteLine(item.ToString());
+            }
+
+            bool czyMoge = true;
+
+        }
+
+        #endregion
+    }
+}
